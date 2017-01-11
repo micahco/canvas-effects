88f4c51325a1a7d5cedefe5cb17e4ac007ad337f
Micah Cowell <micahcowell@gmail.com>
2017-01-10 16:35:59 -0800

    compiling es6 for npm

--- src/index.js
+++ src/index.js
@@ -1,16 +1,5 @@
 import Constellations from './Constellations';
 
-let foo = new Constellations({
-	container: '#foo',
-	width: '100%',
-	height: 400
-});
-foo.init();
-
-
-let bar = new Constellations({
-	container: '#bar',
-	width: 600,
-	height: 600
-});
-bar.init();
+export {
+	Constellations
+}
