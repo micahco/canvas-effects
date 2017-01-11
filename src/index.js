99c46a49b3542849978f3404385f0870d0834e84
Micah Cowell <micahcowell@gmail.com>
2017-01-10 16:02:21 -0800

    cache rules everything around me

--- src/index.js
+++ src/index.js
@@ -3,6 +3,14 @@ import Constellations from './Constellations';
 let foo = new Constellations({
 	container: '#foo',
 	width: '100%',
-	height: '100%'
+	height: 400
 });
 foo.init();
+
+
+let bar = new Constellations({
+	container: '#bar',
+	width: 600,
+	height: 600
+});
+bar.init();
